ddc4b1dd63766755180ef6b84c21abf0b525b6a6
David Blood <davidbjourno@users.noreply.github.com>
2017-10-26 16:06:15 +0100

    Show submit button

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2017-08-17T12:00:00Z'),
 
   headline: 'What would you like to know about getting ahead in your career?',
 
